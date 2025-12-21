ab14d35fc73c175738c997119771e7a3a56d9f8a
Batsandi <geol.aleksandar@gmail.com>
2025-12-21 18:17:46 +0200

    some tailwind use

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -1,8 +1,11 @@
 <script setup></script>
 
 <template>
-  <main>
-    <p class="text-red-200 hover:text-yellow-200">Ti si Bate Sando!</p>
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl text-blue-100">Event Bookings</h1>
+    <h2 class="text-2xl text-blue-100">All Events</h2>
+    <h2 class="text-2xl text-blue-100 font-medium">Your Bookings</h2>
+
   </main>
 </template>
 
