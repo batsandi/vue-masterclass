3258d7b31b1f67e7fa2693f72591cb3a1a1ac9a1
Batsandi <geol.aleksandar@gmail.com>
2025-12-21 18:46:52 +0200

    component styling and list render

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -1,12 +1,16 @@
-<script setup></script>
-
 <template>
-  <main class="container mx-auto my-8 space-y-8">
+  <main class="container mx-auto my-8 space-y-8 space-x-4">
     <h1 class="text-4xl text-blue-100">Event Bookings</h1>
+    <section class="grid grid-cols-3 gap-8">
+      <EventCard v-for="i in 9" :key="i" />
+    </section>
     <h2 class="text-2xl text-blue-100">All Events</h2>
     <h2 class="text-2xl text-blue-100 font-medium">Your Bookings</h2>
-
   </main>
 </template>
+
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+</script>
 
 <style scoped></style>
---
+++ event-booking/src/components/EventCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-blue-100 border border-cyan-500 bg-blue-900 rounded-md">
+    <h3 class="p-3 font-medium border-b border-cyan-400">What</h3>
+    <p class="p-3 text-xs font-mono border-b border-cyan-400">When</p>
+    <p class="p-3 text-xs">Description</p>
+  </div>
+</template>
