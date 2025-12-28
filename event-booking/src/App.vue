0d1ecc87175b891f5741023dd091834fac3414e0
Alexander Georgiev <batsandi@Alexanders-MacBook-Pro.local>
2025-12-28 16:45:28 +0200

    skeletons

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -2,17 +2,19 @@
   <main class="container mx-auto my-2 md:my-8 space-y-8 space-x-4 px-12 md:px-24  py-2 md:py-8">
     <h1 class="text-4xl text-blue-100">Event Bookings</h1>
     <h2 class="text-2xl text-blue-100">All Events</h2>
-    <section class="grid grid-cols-1 md:grid-cols-3 gap-8" v-if="loadingEvents === false">
-      <EventCard v-for="event in events"
-      :key="event.id"
-      :title="event.title"
-      :date="event.date"
-      :description="event.description"
-      @register="title => console.log('Registered!')"
-      />
-    </section>
-    <section v-else class="text-4xl text-lime-400 flex justify-center border border-amber-300 p-3">
-      Loading events...
+    <section class="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <template v-if="loadingEvents === false">
+        <EventCard v-for="event in events"
+        :key="event.id"
+        :title="event.title"
+        :date="event.date"
+        :description="event.description"
+        @register="title => console.log('Registered!')"
+        />
+      </template>
+      <template v-else>
+        <LoadingEventCard v-for="i in 4" :key="i"/>
+      </template>
     </section>
     <h2 class="text-2xl text-blue-100 font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-8">
@@ -23,6 +25,8 @@
 
 <script setup>
 import EventCard from '@/components/EventCard.vue'
+import LoadingEventCard from '@/components/LoadingEventCard.vue'
+
 import BookingCard from '@/components/BookingCard.vue'
 import { ref, onMounted } from 'vue'
 
---
+++ event-booking/src/components/LoadingEventCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <SectionedCard>
+    <template #header>
+      <div class="w-full h-6 bg-gray-400 rounded animate-pulse"></div>
+    </template>
+    <template #default>
+      <div class="w-full h-4 bg-gray-400 rounded animate-pulse"></div>
+    </template>
+    <template #footer>
+      <div class="w-full h-16 bg-gray-400 rounded animate-pulse"></div>
+    </template>
+  </SectionedCard>
+</template>
+
+<script setup>
+import SectionedCard from '@/components/SectionedCard.vue';
+</script>
