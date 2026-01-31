0f9c7d5d79353195dbb7872902392e00ef0856f2
Batsandi <geol.aleksandar@gmail.com>
2026-01-31 14:45:47 +0200

    programmatic routing router.push

--- recipe-book/src/views/AddRecipeView.vue
+++ recipe-book/src/views/AddRecipeView.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        Add Recipe
-    </div>
+  <div>
+    <h1>Add Recipe</h1>
+    <form @submit.prevent="handleAdd">
+      <button type="submit">Add</button>
+    </form>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const handleAdd = () => {
+  router.push({
+    name: 'recipe',
+    params: {
+      id: 1,
+    },
+  });
+};
+</script>
