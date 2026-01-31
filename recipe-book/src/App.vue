2792ee6813d16eaa0a17a7e899ec5fc362e27e90
Batsandi <geol.aleksandar@gmail.com>
2026-01-31 14:08:53 +0200

    random id routing

--- recipe-book/src/App.vue
+++ recipe-book/src/App.vue
@@ -6,10 +6,15 @@
           <RouterLink to="/">Home</RouterLink>
         </li>
         <li>
-          <RouterLink :to="{name: 'favorites'}">Favorites</RouterLink>
+          <RouterLink :to="{ name: 'favorites' }">Favorites</RouterLink>
         </li>
         <li>
           <RouterLink to="/add-recipe">Add Recipe</RouterLink>
+        </li>
+        <li>
+          <RouterLink :to="{ name: 'recipe', params: randRecipeId }" @click="refreshRandom">
+            Random Recipe
+          </RouterLink>
         </li>
       </ul>
     </nav>
@@ -18,5 +23,13 @@
 </template>
 
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
+import { RouterLink, RouterView } from 'vue-router';
+import { ref } from 'vue';
+
+const randRecipeId = ref({ id: Math.floor(Math.random() * 50) + 1 });
+
+const refreshRandom = () => {
+  randRecipeId.value = { id: Math.floor(Math.random() * 50) + 1 };
+};
+
 </script>
--- recipe-book/src/views/RecipeView.vue
+++ recipe-book/src/views/RecipeView.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
-        {{ $route.params.id  }}
-    </div>
+  <div>
+    {{ $route.params.id }}
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+console.log(route.params.id)
+</script>
