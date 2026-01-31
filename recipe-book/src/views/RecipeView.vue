2c875dd9622b22a009b152a507fe196892989096
Batsandi <geol.aleksandar@gmail.com>
2026-01-31 15:07:25 +0200

    reactivity on same-component routing

--- recipe-book/src/views/RecipeView.vue
+++ recipe-book/src/views/RecipeView.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
-    {{ $route.params.id }}
+    <h1>{{ $route.params.id }}</h1>
+    <nav>
+      <ul>
+        <li>
+          <RouterLink :to="{ name: 'recipe', params: { id: 1 } }"> Recipe 1 </RouterLink>
+        </li>
+        <li>
+          <RouterLink :to="{ name: 'recipe', params: { id: 2 } }"> Recipe 2 </RouterLink>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
 <script setup lang="ts">
+import { watch } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 
-console.log(route.params.id)
+watch(
+  () => route.params.id,
+  () => console.log(`visiting: ${route.params.id}`),
+);
 </script>
